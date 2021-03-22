3d3896763b979848143c52fba3e933b01d1a29e2
Eric Mitchell <ericmitchell42@gmail.com>
2021-03-21 19:21:28 -0600

    Setup tests

--- test/people.test.js
+++ test/people.test.js
@@ -4,6 +4,33 @@ const app = require("../src/app");
 describe("Test the people path", () => {
   test("It should get an array of starwars people", async () => {
     const response = await request(app).get("/people");
+    
     expect(response.statusCode).toBe(200);
+    expect(response.body.people.length).toBeGreaterThan(10)
+    expect(response.body).toMatchSnapshot()
+  });
+  
+  test("It should sort by name", async () => {
+    const response = await request(app).get("/people?sortBy=name");
+    
+    expect(response.statusCode).toBe(200);
+    expect(response.body.people.length).toBeGreaterThan(10)
+    expect(response.body).toMatchSnapshot()
+  });
+  
+  test("It should sort by height", async () => {
+    const response = await request(app).get("/people?sortBy=height");
+    
+    expect(response.statusCode).toBe(200);
+    expect(response.body.people.length).toBeGreaterThan(10)
+    expect(response.body).toMatchSnapshot()
+  });
+  
+  test("It should sort by mass", async () => {
+    const response = await request(app).get("/people?sortBy=mass");
+    
+    expect(response.statusCode).toBe(200);
+    expect(response.body.people.length).toBeGreaterThan(10)
+    expect(response.body).toMatchSnapshot()
   });
 });
--- test/planets.test.js
+++ test/planets.test.js
@@ -5,5 +5,8 @@ describe("Test the planets path", () => {
   test("It should get an array of starwars planets", async () => {
     const response = await request(app).get("/planets");
     expect(response.statusCode).toBe(200);
+    expect(response.body.planets.length).toBeGreaterThan(10)
+
+    expect(resonse.boyd.planets[0].residents[0]).toBe('Leia Organa')
   });
 });
